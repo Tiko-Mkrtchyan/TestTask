0b7bce160a7e24f378113bba06c4680745fd51aa
Tiko-Mkrtchyan <76784923+Tiko-Mkrtchyan@users.noreply.github.com>
2024-12-12 15:21:42 +0400

    added bill collecting,bottle collecting. Added doors and animations

--- TestTask/Assets/Scripts/Player/Player.cs
+++ TestTask/Assets/Scripts/Player/Player.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections;
 using Level_Manager;
 using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using DG.Tweening;
 
 namespace Player
 {
@@ -11,22 +13,49 @@ namespace Player
         [SerializeField] private float turnSpeed;
         [SerializeField] private float walkSpeed;
         [SerializeField] private TextMeshProUGUI hintText;
-        [SerializeField] private GameManager _gameManager;
+        [SerializeField] private GameManager gameManager;
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private AudioClip coinSound;
+        [SerializeField] private AudioClip bottleSound;
+        [SerializeField] private AudioClip stepSound;
+        [SerializeField] private TextMeshProUGUI coinText;
+        [SerializeField] private TextMeshProUGUI bottleText;
+        [SerializeField] private GameObject avatarCasual;
+        [SerializeField] private GameObject avatarMiddle;
+        [SerializeField] private GameObject avatarBling;
+
         private Animator _playerAnimator;
+        private event Action OnMoneyCollected;
+        private event Action OnBottleCollected;
+        private readonly float _stepSoundCooldown = 0.7f;
+        private float _stepSoundTimer = 0f;
+        private Vector2 _coinTextStartPosition;
+        private Vector2 _bottleTextPosition;
+
 
         public bool died;
         public int money;
         public bool gameStarted;
 
+
         private void OnEnable()
         {
-            _gameManager.OnGameOver += OnDie;
+            gameManager.OnGameOver += OnDie;
+            OnMoneyCollected += CollectMoney;
+            OnBottleCollected += CollectBottle;
         }
 
         private void Awake()
         {
+            _coinTextStartPosition=coinText.rectTransform.anchoredPosition;
+            _bottleTextPosition = bottleText.rectTransform.anchoredPosition;
+            coinText.gameObject.SetActive(false);
+            bottleText.gameObject.SetActive(false);
             hintText.enabled = true;
             _playerAnimator = GetComponent<Animator>();
+            avatarBling.SetActive(false);
+            avatarMiddle.SetActive(false);
+            avatarCasual.SetActive(true);
         }
 
         private void Update()
@@ -39,7 +68,7 @@ namespace Player
 
             if (money <= 0)
             {
-                _gameManager.gameOver = true;
+                gameManager.gameOver = true;
             }
 
             if (gameStarted)
@@ -74,6 +103,16 @@ namespace Player
             if (!died)
             {
                 transform.Translate(Vector3.forward * (walkSpeed * Time.deltaTime));
+
+                if (_stepSoundTimer <= 0f)
+                {
+                    PlayStepSound();
+                    _stepSoundTimer = _stepSoundCooldown;
+                }
+                else
+                {
+                    _stepSoundTimer -= Time.deltaTime;
+                }
             }
         }
 
@@ -87,18 +126,84 @@ namespace Player
         {
             if (other.gameObject.CompareTag("Bill"))
             {
-                var randomAmount = Random.Range(0, 4);
-                money += randomAmount;
+                OnMoneyCollected?.Invoke();
+                Destroy(other.gameObject);
             }
             else if (other.gameObject.CompareTag("Bottle"))
             {
-                money -= 20;
+                OnBottleCollected?.Invoke();
+                Destroy(other.gameObject);
+            }
+
+            if (other.gameObject.CompareTag("Door"))
+            {
+                other.GetComponent<Animator>().enabled = true;
+            }
+        }
+
+        private void CollectMoney()
+        {
+            var randomAmount = Random.Range(2, 6);
+            money += randomAmount;
+            PlayAudio(coinSound);
+            coinText.gameObject.SetActive(true);
+            coinText.text = "+ " + randomAmount + " $";
+            coinText.rectTransform.DOAnchorPosY(coinText.rectTransform.anchoredPosition.y + 30, 0.5f);
+            StartCoroutine(WaitToDestroy(coinText.gameObject));
+            coinText.rectTransform.anchoredPosition = _coinTextStartPosition;
+            if (money>=60)
+            {
+                avatarCasual.SetActive(false);
+                avatarMiddle.SetActive(true);
+            }
+
+            if (money>=90)
+            {
+                avatarMiddle.SetActive(false);
+                avatarBling.SetActive(true);
+            }
+        }
+
+        private void CollectBottle()
+        {
+            money -= 20;
+            PlayAudio(bottleSound);
+            bottleText.gameObject.SetActive(true);
+            bottleText.text = "- 20 $";
+            bottleText.rectTransform.DOAnchorPosY(bottleText.rectTransform.anchoredPosition.y + 30, 0.5f);
+            StartCoroutine(WaitToDestroy(bottleText.gameObject));
+            bottleText.rectTransform.anchoredPosition = _bottleTextPosition;
+        }
+
+        private IEnumerator WaitToDestroy(GameObject objectToDestroy)
+        {
+            yield return new WaitForSeconds(1f);
+            objectToDestroy.SetActive(false);
+        }
+
+        private void PlayAudio(AudioClip clip)
+        {
+            if (clip != null)
+            {
+                audioSource.clip = clip;
+                audioSource.Play();
+            }
+        }
+
+        private void PlayStepSound()
+        {
+            if (stepSound != null)
+            {
+                audioSource.clip = stepSound;
+                audioSource.Play();
             }
         }
 
         private void OnDisable()
         {
-            _gameManager.OnGameOver -= OnDie;
+            gameManager.OnGameOver -= OnDie;
+            OnMoneyCollected -= CollectMoney;
+            OnBottleCollected -= CollectBottle;
         }
     }
 }
